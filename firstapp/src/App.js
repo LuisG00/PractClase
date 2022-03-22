60d8ad151ed7ee01e0dfbc6d92c270b581012e93
LuisG00 <gutierrezluis6645@gmail.com>
2022-03-22 13:59:39 -0600

    clase viernes

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,36 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import Greetings from './components/Greetings';
+// import Greetings from './components/Greetings';
 
 function App() {
-  const objeto ={
-    grupo: ' 8vo A Informática',
-    day: ' Viernes'
+  const [stateClass, setStateClass] = useState(true)
+  const [contar, setContar] = useState(0)
+
+  useEffect(() => {
+    console.log('Total: ' + contar)
+  }, [contar])
+//   const objeto ={
+//     grupo: ' 8vo A Informática',
+//     day: ' Viernes'
+//   }
+//   const saludo = (d, g) => {
+//    // alert('Excelente' + d + '' + g)
+//    alert(`Excelente ${d} ${g}`)
+// }
+
+  const iniciarTerminar = () => {
+    setStateClass(!stateClass)
+    setContar(contar + 1)
   }
-  const saludo = (g) => {
-    alert('Excelente día' + g)
-}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greetings grupoInf={objeto} funcion = {saludo} />
+        {/* <Greetings grupoInf={objeto} funcion = {saludo} /> */}
+        <h2>La clase ha {stateClass?'Iniciado':'Terminado'}</h2>
+        <h3>Clicks: {contar}</h3>  
+        <button onClick={iniciarTerminar}>Iniciar/Terminar</button>
       </header>
     </div>
   );
--- firstapp/src/components/Greetings.js
+++ firstapp/src/components/Greetings.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Greetings(props){
     const {grupoInf, funcion} = props
-    const {grupo, day} = grupoInf
+    const {grupo='Invitado', day} = grupoInf
     console.log(props)
     console.log(grupoInf)
     console.log(grupo)
     console.log(day)
     return(
         <div>
-            <button onClick={() => funcion(grupo)}>Greetings</button>
+            <button onClick={() => funcion(day, grupo)}>Greetings</button>
         </div>
     )
 }
